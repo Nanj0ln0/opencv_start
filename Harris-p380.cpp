75c69cb87717938de69f2b6c21f1b024c54a92ec
Nanj0ln0 <2220799783@qq.com>
2022-05-17 21:44:24 +0300

    1

--- Harris-p380.cpp
+++ Harris-p380.cpp
@@ -3,6 +3,7 @@
 
 using namespace cv;
 
+
 int main() {
 
 	Mat src = imread("C:\\Users\\22207\\Desktop\\superis\\text.JPG",0);
@@ -14,7 +15,15 @@ int main() {
 	namedWindow("imshow",CV_WINDOW_AUTOSIZE);
 	imshow("imshow",src);
 
+	Mat cornerStrength;
+	cornerHarris(src,cornerStrength,2,3,0.01);
 
+	Mat harrisCorner;
+	threshold(cornerStrength,harrisCorner,0.00001,255,THRESH_BINARY);
+
+	namedWindow("output",CV_WINDOW_AUTOSIZE);
+	imshow("output", harrisCorner);
+	
 
 	waitKey(0);
 	return 0;
