1bf8755606c265a8e726da53366fb3c9d3cc18e8
Nanj0ln0 <2220799783@qq.com>
2022-05-24 15:26:38 +0300

    1

--- HOG.cpp
+++ HOG.cpp
@@ -9,7 +9,7 @@ using namespace cv::xfeatures2d;
 const char* input_title = "input";
 
 int main() {
-	Mat src = imread("C:/Users/22207/Desktop/superis/man.jpg");
+	Mat src = imread("C:/Users/22207/Desktop/superis/mans-2.jpg");
 	if (!src.data)
 	{
 		printf("ERROR");
@@ -19,6 +19,8 @@ int main() {
 	namedWindow(input_title,CV_WINDOW_AUTOSIZE);
 	imshow(input_title,src);
 
+	//简单实现HOG检测
+	/*
 	Mat src_resize;
 	resize(src,src_resize,Size(64,128));
 
@@ -31,6 +33,21 @@ int main() {
 	
 	detector.compute(src_gray,descriptors,Size(0,0),Size(0,0),locations);
 	printf("特征点有%d个",descriptors.size());
+	*/
+	
+	HOGDescriptor hog = HOGDescriptor();
+	hog.setSVMDetector(hog.getDefaultPeopleDetector());
+
+	vector<Rect> foundLocations;
+	hog.detectMultiScale(src,foundLocations,0,Size(8,8),Size(32,32),1.05,2);
+	for (size_t t = 0; t < foundLocations.size(); t++)
+	{
+		rectangle(src,foundLocations[t],Scalar(0,0,255),2,8,0);
+
+	}
+
+	namedWindow("HOG+SVM", CV_WINDOW_AUTOSIZE);
+	imshow("HOG+SVM",src);
 
 
 	waitKey(0);
