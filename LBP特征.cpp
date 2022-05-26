f073cb7673aa1d03b6dde721905ea374c4787a05
Nanj0ln0 <2220799783@qq.com>
2022-05-26 17:08:34 +0300

    添加项目文件。

---
+++ LBP特征.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/xfeatures2d.hpp>
+
+using namespace cv;
+using namespace std;
+using namespace cv::xfeatures2d;
+
+const char* input = "input_picture";
+
+int	main() {
+	Mat src = imread("C:/Users/22207/Desktop/superis/mans-2.jpg");
+	if (!src.data)
+	{
+		printf("ERROR");
+		return -1;
+	}
+	namedWindow(input, CV_WINDOW_AUTOSIZE);
+	imshow(input,src);
+
+	waitKey(0);
+	return 0;
+}
