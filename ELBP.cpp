acf0961da0b5bdc532cac24a3f6fd1ba6b53acc3
Nanj0ln0 <2220799783@qq.com>
2022-05-26 21:26:45 +0300

    0

---
+++ ELBP.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/xfeatures2d.hpp>
+
+using namespace cv;
+using namespace std;
+using namespace cv::xfeatures2d;
+
+
+int min;
+int max;
+
+
+int main() {
+
+
+
+
+}
