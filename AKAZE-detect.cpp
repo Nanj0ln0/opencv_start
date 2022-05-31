7a3b3d04f6f6571fcd760f9a26a688199cd932c5
Nanj0ln0 <2220799783@qq.com>
2022-05-31 11:15:59 +0300

    0

--- AKAZE-detect.cpp
+++ AKAZE-detect.cpp
@@ -5,25 +5,31 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat src = imread("C:/Users/22207/Desktop/superis/jianzhuwu.JPG",0);
-	if (!src.data)
+	Mat obj = imread("C:/Users/22207/Desktop/superis/pipei0 - 1.JPG",0);
+	Mat scene = imread("C:/Users/22207/Desktop/superis/pipei0.JPG", 0);
+	if (!obj.data||!scene.data)
 	{
 		printf("ERROR");
 		return -1;
 	}
-	namedWindow("input",CV_WINDOW_AUTOSIZE);
-	imshow("input",src);
-
+	
+	imshow("obj",obj);
+	imshow("scene", scene);
 
 	//kaze detection
 	Ptr<AKAZE>detector = AKAZE::create();
-	vector<KeyPoint>KeyPoints;
-	detector->detect(src,KeyPoints,Mat());
+	vector<KeyPoint>KeyPoints_obj;
+	vector<KeyPoint>KeyPoints_scene;
+	Mat descritor_obj, descritor_scene;  //√Ë ˆ◊”
+	detector->detectAndCompute(obj,Mat(),KeyPoints_obj,descritor_obj);
+	detector->detectAndCompute(scene,Mat(),KeyPoints_scene,descritor_scene);
 
-	Mat keypointImg;
-	namedWindow("keypointImg",CV_WINDOW_AUTOSIZE);
-	drawKeypoints(src,KeyPoints,keypointImg,Scalar::all(-1),DrawMatchesFlags::DEFAULT);
-	imshow("keypointImg",keypointImg);
+	//match flann∆•≈‰  √Ë ˆ◊”
+	FlannBasedMatcher matcher;
+	vector<DMatch> matches;
+	matcher.match(descritor_obj,descritor_scene,matches);
+	
+	
 
 	waitKey(0);
 	return 0;
